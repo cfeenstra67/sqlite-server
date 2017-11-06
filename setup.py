3158c15e668c328f889839eb21727260fddac6f4
cfeenstra67 <cameron.l.feenstra@gmail.com>
2017-11-05 22:41:44 -0600

    fixed setup script

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
 	description='Simple sqlite database server using twisted',
 	author='Cam Feenstra',
 	author_email='cameron.l.feenstra@gmail.com',
-	packages=['sqlite-server'],
+	packages=['sqlite_server'],
 	install_requires=['Twisted', 'pandas']
 )
